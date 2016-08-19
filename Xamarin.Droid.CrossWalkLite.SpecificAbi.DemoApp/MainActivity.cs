45b154d49f828edacdf1c73a6c28cb13b2c8557b
Yann <me.show@gmail.com>
2016-08-19 14:53:09 +0200

    add demo app

---
+++ Xamarin.Droid.CrossWalkLite.SpecificAbi.DemoApp/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Xamarin.Droid.CrossWalkLite.SpecificAbi.DemoApp
+{
+    [Activity(Label = "Xamarin.Droid.CrossWalkLite.SpecificAbi.DemoApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Org.Xwalk.Core.XWalkActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+
+        protected override void OnXWalkReady()
+        {
+            var view = new RelativeLayout(this.BaseContext);
+            var mp = ViewGroup.LayoutParams.MatchParent;
+            var xwv = new Org.Xwalk.Core.XWalkView(this.BaseContext, this);
+            view.AddView(xwv);
+            this.AddContentView(view, new ViewGroup.LayoutParams(mp, mp));
+
+            xwv.Load("http://www.google.com", null);
+        }
+    }
+}
+
